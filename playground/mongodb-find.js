194345224f502a1d907357a2894df10f73f322b8
Eric Xie <xiezhijay@gmail.com>
2018-06-02 23:30:37 +1000

    init commit
    -todo apis implemented by node.js

---
+++ playground/mongodb-find.js
@@ -0,0 +1,21 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+console.log(ObjectID);
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+    db.collection('Todos').find({
+        _id: new ObjectID('5b10c54881c9432579da3a77')
+    }).toArray().then((docs)=>{
+        console.log('Todos');
+        console.log(JSON.stringify(docs,undefined,2));
+    },(err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+});
+
+console.log(ObjectID);
---
+++ server/server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
+
+
+var {mongoose} = require('./db/mongoose');
+
+var{Todo} = require('./model/todo');
+var{User} = require('./model/user');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/todos',(req, res) =>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+
+    },(e) => {
+        res.status(400).send(e);
+    })
+});
+
+
+app.get('/todos/:id', (req, res) =>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(400).send('');
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        else{
+            res.send({todo});
+        }
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+
+});
+
+app.post('/todos',(req, res) => {
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+    
+    todo.save().then((doc) => {
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
+
+
+app.listen(3000,()=>{
+    console.log(`started on port ${port}`);
+});
+
+
+module.exports = {app};
