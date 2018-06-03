4f9323e13721b9ba02d8a994c30f7b6574a030bb
Eric Xie <xiezhijay@gmail.com>
2018-06-03 15:43:40 +1000

    Add Delete route /todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,6 +57,26 @@ app.post('/todos',(req, res) => {
 });
 
 
+app.delete('/todos/:id',(req, res)=>{
+    var id = req.params.id;
+    console.log('id', id);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('invalid id');
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        
+        res.status(200).send(todo);
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
+
 
 
 app.listen(port,()=>{
